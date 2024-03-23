41524c61b14024446dbb3d5be3adbb23d1d10ad6
iAMMIA <iammia4tp@gmail.com>
2024-03-23 18:58:11 +0700

    23/03/24 06:57 PM

--- server/server.js
+++ server/server.js
@@ -20,7 +20,34 @@ app.get('/api', (req, res) => {
     res.send("Welcome to my API!");
 })
 
-//api for 
+//api for classification the drug
+app.post('/upload', async(req, res) => {
+    try{
+        //read img from request
+        const imgData = req.body.image;
+
+        //convert imgData to tensor
+        const imgTensor = tf.node.decodeImage(new Uint8Array(imgData), 3);
+        const resizeImg = tf.image.resizeBilinear(imgTensor, [224, 224]);
+
+        //normalize img
+        const normalizeImg = resizeImg.div(tf.scalar(255));
+
+        //make prediction
+        const prediction = model.predict(normalizeImg.expandDims(0));
+        const result = quediction.dataSync();
+
+        //get predicted label (assuming the model output is softmax)
+        const predictedLabelIndex = result.indexOf(Math.max(...result));
+
+        //send response with predicted label
+        res.json({result: predictedLabelIndex});
+
+    } catch(error) {
+        console.error('Error processing image: ', error);
+        res.status(500).json({error: 'Error processing image'});
+    }
+})
 
 //start the server
 app.listen(port, () => {
