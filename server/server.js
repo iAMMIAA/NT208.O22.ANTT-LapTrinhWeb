65dff1ac9dac4afcf4941f542c747ba400be6307
iAMMIA <iammia4tp@gmail.com>
2024-03-23 18:54:51 +0700

    23/02/24 06:54 PM

--- server/server.js
+++ server/server.js
@@ -8,11 +8,21 @@ const app = express();
 const port = 3000;
 
 //Load mode
-const MODEL_PATH = '';
+const MODEL_PATH = './ResNet18.pth';
+let model;
+(async () => {
+    model = await tf.loadLayersModel('file://${MODEL_PATH}');
+    console.log('Model loaded');
+})();
 
 //define a route for your API
 app.get('/api', (req, res) => {
     res.send("Welcome to my API!");
 })
 
-//st
+//api for 
+
+//start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
