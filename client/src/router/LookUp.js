f3e5d05e872d8a7a48f78498a017ae6638b043b7
iAMMIA <iammia4tp@gmail.com>
2024-03-24 03:21:08 +0700

    Finish LookUp 24/03/24 03:21 AM

--- client/src/router/LookUp.js
+++ client/src/router/LookUp.js
@@ -5,74 +5,77 @@ import bieu3 from './pictures/bieu3.jpg';
 import drugImg from './pictures/drug.png'
 
 function LookUp() {
-    const [file, setFile] = useState(null);
-    const [drugName, setDrugName] = useState('');
-    const [drugImage, setDrugImage] = useState('');
-  
-    const handleFileChange = (event) => {
-      setFile(event.target.files[0]);
-      setDrugImage(URL.createObjectURL(event.target.files[0]));
-    };
-  
-    const uploadImage = () => {
-      const formData = new FormData();
-      formData.append('image', file);
-  
-      axios.post('http://127.0.0.1:8000/api/create/', formData)
-        .then((response) => {
-          const drug = response.data;
-          const idDrug = drug.idDrug;
-          const fileImages = drug.picture;
-          const linkImage = `http://127.0.0.1:8000${fileImages}/`;
-  
-          setDrugImage(linkImage);
-  
-          axios.get(`http://127.0.0.1:8000/api/detail/${idDrug}/`)
-            .then((response) => {
-              const drug = response.data;
-              setDrugName(drug.name);
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    };
-  
-    const handleCheck = (event) => {
-      event.preventDefault();
-      uploadImage(); // Gọi hàm uploadImage để gửi ảnh lên máy chủ và hiển thị kết quả
-    };
+  const[selectedImage, setSelectedImage] = useState(null);
+  const[resultDrug, setResultDrug] = useState(null);
+  const[detailDrug, setDetailDrug] = useState(null);
+  const[sourceInfo, setSourceInfo] = useState(null);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file)
+  };
+
+  const handleLookUp = () =>{
+    if(!selectedImage){
+      alert("Vui long chon hinh anh truoc khi tra cuu");
+      return;
+    } 
     
-    return (
-        <div className='LookUp'>
-            <div class = "uploadImage">
-                <form class="drugForm" id="drugForm" encType="multipart/form-data">
-                    <div class="input_group">
-                        <input type="file" class="form_control" id="imageInput" name="image" onChange={handleFileChange} />
-                        <label class="input_group_text" for="inputGroupFile02" onSubmit={uploadImage}>Tải ảnh lên</label>
-                    </div>
-                    <br />
-                </form>
-                <button type="button" id="btnCheck" class="btn_check" onClick={handleCheck}>Tra cứu</button>
-            </div>
-            <div className="result">
-                <div className='square1' style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <img src={bieu3}></img>
+    const formData = new FormData();
+    formData.append('image', selectedImage);
+
+    axios.post('http://localhost:3001/predict', formData)
+      .then(Response => {
+        // setResultDrug(Response.data);
+        const drug = Response.data;
+        const nameDrug = drug.nameDrug;
+        const cite = drug.cites;
+        const detailDrug = drug.detail;
+        setResultDrug(nameDrug);
+        setDetailDrug(detailDrug);
+        setSourceInfo(cite);
+
+      })
+      .catch(error => {
+        console.error("Error while fetching result: ", error);
+      });
+  };
+    
+  return (
+      <div className='LookUp'>
+          <div class = "uploadImage">
+              <form class="drugForm" >
+                  <div class="input_group">
+                      <input type="file" class="form_control" placeholder="Tải ảnh lên" onChange={handleImageChange} name='image'/>
+                      <label class="input_group_text">Tải ảnh lên</label>
+                  </div>
+                  <br/>
+              </form>
+              <button type="button" class="btn_check" onClick={handleLookUp}>Tra cứu</button>
+          </div>
+          <div className="result">
+              <div className='square1' style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <div className='information_drug'>
+                  <p className='detail_drug'>{detailDrug}</p>
+                  <p className='source_info_drug'>Trích dẫn: "{sourceInfo}"</p>
                 </div>
-                <div className='rectangle'>
-                    <div className='square2'>
-                        <p className='line1'>Drug predicted:</p>
-                        <p className='resultDrug1' id="decor"><h2 className='resultDrug' id="result">"Drug's name"</h2></p>
-                    </div>
-                    <div className='square3'>
-                        <img className='drugImage' id="drugImage" src={drugImage} alt="Drug" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-  }
+                  
+              </div>
+              <div className='rectangle'>
+                  <div className='square2'>
+                      <p className='line1'>Drug predicted:</p>
+                      <p className='resultDrug1'>
+                        <h2 className='resultDrug' id="result">{resultDrug}</h2>
+                      </p>
+                  </div>
+                  <div className='square3'>
+                      {selectedImage && 
+                        <img className='drugImage' alt="Drug" src={URL.createObjectURL(selectedImage)}/>
+                      }
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
 export default LookUp;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require('cors')
 const multer =  require('multer'); // Thư viện multer để xử lý dữ liệu hình ảnh
 const { spawn } = require('child_process');
 const mysql = require('mysql2');
 const { error } = require('console');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-//Home
+//su dung CORS Middleware
+app.use(cors());
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
@@ -15,6 +18,41 @@ const connection = mysql.createConnection({
     password: 'i.AMMIAK16',
     database: 'DrugWeb'
 });
+
+//LookUp
+// Thiết lập multer để lưu trữ hình ảnh tạm thời trong thư mục uploads
+const upload = multer({dest: 'uploads/'});
+app.post('/predict', upload.single('image'), (req, res) => {
+    const imagePath = req.file.path;
+    const pythonProcess = spawn('python', ['predict_drug.py', imagePath]);
+
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+
+        const nameDrug = data.toString().trim();
+        const query = 'select * from InformationDrug where nameDrug = ?';
+        connection.query(query, [nameDrug], (error, results) => {
+            if(error) {
+                console.error('Lỗi khi truy vấn cơ sở dữ liệu:', error);
+                res.status(500).json({error: 'Loi khi truy van co so du lieu.'});
+            }
+            else {
+                if (results.length > 0) res.json(results[0]);
+                else res.status(404).json({ error: 'Không tìm thấy thông tin thuốc.' });
+            }
+        })
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+});
+
+//Home
 app.get('/posts/:id', (req, res) => {
     const postID = req.params.id;
     const query = 'select * from POSTS where id = ?';
@@ -29,27 +67,6 @@ app.get('/posts/:id', (req, res) => {
                 res.status(404).json({ error: 'Không tìm thấy bài viết' });
             }
         }
-    });
-});
-
-//LookUp
-// Thiết lập multer để lưu trữ hình ảnh tạm thời trong thư mục uploads
-const upload = multer({dest: 'uploads/'});
-app.post('/predict', upload.single('image'), (req, res) => {
-    const imagePath = req.file.path;
-    const pythonProcess = spawn('python', ['predict_drug.py', imagePath]);
-
-    pythonProcess.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-        res.send(data); 
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    pythonProcess.on('close', (code) => {
-        console.log(`Child process exited with code ${code}`);
     });
 });
 
